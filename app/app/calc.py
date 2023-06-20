ef8b16d62df0d5eb25c2d627e3f4036dcf76dbc7
luisalichfett <luisalichfett@gmail.com>
2023-06-20 15:58:55 -0300

    PostgresSQL database setup and adaptor

---
+++ app/app/calc.py
@@ -0,0 +1,14 @@
+"""
+Calculator functions
+"""
+
+
+def add(x, y):
+    """Add x and y return result."""
+    return x + y
+
+
+def subtract(x, y):
+    """Subtract y from x return result."""
+
+    return x - y

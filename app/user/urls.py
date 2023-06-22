a1b3d9ec3071afce3669018550f540eee14466c4
luisalichfett <luisalichfett@gmail.com>
2023-06-22 17:06:13 -0300

    Manage private user and update it.

--- app/user/urls.py
+++ app/user/urls.py
@@ -12,4 +12,5 @@ app_name = 'user'
 urlpatterns = [
     path('create/', views.CreatedUserView.as_view(), name='create'),
     path('token/', views.CreateTokenView.as_view(), name='token'),
+    path('me/', views.ManageUserView.as_view(), name='me'),
 ]

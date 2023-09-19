e8696fd1dae5e1698c586cb5e8a39036bd1738b6
EC2 Default User <ec2-user@ip-172-31-36-115.ec2.internal>
2023-09-19 14:23:55 +0000

    Server health check

---
+++ app/core/tests/test_health_check.py
@@ -0,0 +1,20 @@
+"""
+Tests for the health check API. 
+"""
+
+from django.tests import TestCase
+from django.urls import reverse
+
+from rest_framework import status
+from rest_framework.test import APIClient
+
+class HealthCheckTests(TestCase):
+    """Test the health check API."""
+
+    def text_health_check(self):
+        """Test health check API."""
+
+        client = APIClient()url = reverse('heatlh-check')
+        res = client.get(url)
+
+        self.assertEqual(res.status, status.HTTP_200_OK)
